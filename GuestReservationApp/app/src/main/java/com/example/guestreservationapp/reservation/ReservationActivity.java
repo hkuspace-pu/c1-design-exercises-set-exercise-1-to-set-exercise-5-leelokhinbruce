10a25847841f25152cbd8a52ce8d70ab06cb8fa5
unknown <Administrator@ED3I24A.LOCAL>
2025-11-27 17:31:00 +0800

    guest app - booking form (done)

--- GuestReservationApp/app/src/main/java/com/example/guestreservationapp/reservation/ReservationActivity.java
+++ GuestReservationApp/app/src/main/java/com/example/guestreservationapp/reservation/ReservationActivity.java
@@ -1,15 +1,17 @@
 package com.example.guestreservationapp.reservation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.cardview.widget.CardView;
-
 import com.example.guestreservationapp.R;
 import com.example.guestreservationapp.databinding.ActivityReservationBinding;
+import com.example.guestreservationapp.mainpage.MainActivity;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,8 +31,9 @@ public class ReservationActivity extends AppCompatActivity {
                 new Pair<>("Nov 8", "Sun"),
                 new Pair<>("Nov 9", "Mon")
         );
-
         List<String> times = Arrays.asList("16:00", "16:30", "17:00", "17:30", "18:00", "18:30");
+        List<Integer> guests = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<String> occasions = Arrays.asList("Birthday", "Friends Gathering", "Family Gathering", "Celebration", "Dating");
 
         // Loop through the data and inflate card views
         for (Pair<String, String> item : datesAndLabels) {
@@ -63,5 +66,40 @@ public class ReservationActivity extends AppCompatActivity {
 
             binding.timeCardContainer.addView(cardView);
         }
+
+        // Loop through the guest and inflate card views
+        for (Integer guest : guests) {
+            CardView cardView = (CardView) getLayoutInflater().inflate(R.layout.guest_cardview,
+                    binding.guestCardContainer, false);
+
+            // Reference in the time_cardview.xml
+            AppCompatTextView guestText = cardView.findViewById(R.id.textGuest);
+
+            // Set value
+            guestText.setText(guest);
+
+            binding.guestCardContainer.addView(cardView);
+        }
+
+        // Loop through the guest and inflate card views
+        for (String occasion : occasions) {
+            CardView cardView = (CardView) getLayoutInflater().inflate(R.layout.occasion_cardview,
+                    binding.occasionCardContainer, false);
+
+            // Reference in the time_cardview.xml
+            AppCompatTextView occasionText = cardView.findViewById(R.id.textOccasion);
+
+            // Set value
+            occasionText.setText(occasion);
+
+            binding.occasionCardContainer.addView(cardView);
+        }
+
+        // Close Editing Form and back to mainpage
+        binding.imgBtnClose.setOnClickListener(viewClose -> {
+            Intent intent = new Intent(ReservationActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
     }
 }
